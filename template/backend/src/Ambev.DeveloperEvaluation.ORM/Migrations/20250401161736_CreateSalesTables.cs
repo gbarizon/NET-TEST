653085397d67a861df60324e84d211ec7b441186
Glaucio pançardes <gbarizon@outlook.com>
2025-04-01 13:17:57 -0300

    Ajustando o Migration Assembly

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250401161736_CreateSalesTables.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSalesTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "sales",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    sale_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    branch_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    branch_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    cancelled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sales", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sale_items",
+                columns: table => new
+                {
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    sale_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    discount = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    sale_id1 = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sale_items", x => new { x.product_id, x.sale_id });
+                    table.ForeignKey(
+                        name: "FK_sale_items_sales_sale_id1",
+                        column: x => x.sale_id1,
+                        principalTable: "sales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sale_items_sale_id1",
+                table: "sale_items",
+                column: "sale_id1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sale_items");
+
+            migrationBuilder.DropTable(
+                name: "sales");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Users");
+        }
+    }
+}
